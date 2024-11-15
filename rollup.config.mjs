852117555320315ba97142ad91c4ce58ab1f3468
charJKL <11600464+charJKL@users.noreply.github.com>
2024-11-15 09:36:10 +0100

    Build settings. Copy asset files.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,12 +2,27 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss'
+import del from 'rollup-plugin-delete'
+import copy from 'rollup-plugin-copy'
 
 const postcssConfig = 
 {
 	extract: true,
   modules: true,
 }
+const delConfig =
+{
+	targets: ['./build/*']
+}
+const copyConfigBackground = 
+{
+	targets: [ { src: "./src/background/manifest.json", dest:"./build" } ]
+}
+const copyConfigForegroundSettings =
+{
+	targets: [ {src: "./src/foreground/html/settings/*.html", dest: "./build/html/settings"}]
+}
+
 
 export default 
 [
@@ -18,7 +33,7 @@ export default
 			file: "build/background.js",
 			format: "es",
 		},
-		plugins: [typescript()]
+		plugins: [commonjs(), typescript(),nodeResolve(), del(delConfig), copy(copyConfigBackground)]
 	},
 	{
 		input: "./src/foreground/html/settings/settings.tsx",
@@ -27,6 +42,7 @@ export default
 			file: "./build/html/settings/settings.js",
 			format: "es",
 		},
-		plugins: [commonjs(), typescript(), nodeResolve(), postcss(postcssConfig)]
+		plugins: [commonjs(), typescript(), nodeResolve(), postcss(postcssConfig), copy(copyConfigForegroundSettings)]
 	}
 ]
+
